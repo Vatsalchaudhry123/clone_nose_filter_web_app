550768e9691848e6cbac3cf6e5ebb0973f287cb7
Vatsalchaudhry123 <117192978+Vatsalchaudhry123@users.noreply.github.com>
2023-01-30 20:52:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+nosex=0
+nosey=0
 function setup()
 {
     Canvas=createCanvas(300,300)
@@ -19,13 +21,18 @@ function modelloaded()
 function draw()
 {
     image(video,0,0,300,300)
+    image(clown_nose,nosex,nosey,30,30)
 }
 function gotposes(results)
 {
     if(results.length>0)
     {
         console.log(results)
-        console.log("nosex"+results[0].pose.nose.x)
-        console.log("nosey"+results[0].pose.nose.y)
+        nosex=results[0].pose.nose.x-15
+        nosey=results[0].pose.nose.y-15
     }
+    }
+    function preload()
+    {
+        clown_nose=loadImage("https://i.postimg.cc/7hThby7q/clown.jpg")
     }
